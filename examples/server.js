f2ed1f1a682e7397430a32114d4754d6a32629d3
Ben Augarten <b.augarten@gmail.com>
2013-01-27 12:54:40 -0800

    Added example of options in user routes

--- examples/server.js
+++ examples/server.js
@@ -40,7 +40,28 @@ app.register({
       res.write(JSON.stringify({
         hello: "HIIII",
       }));
-    }
+    },
+    anotherroute: {
+      handler: function(req, res, next) {
+        res.writeHead(200, {'Content-Type': 'application/json' });
+        res.write(JSON.stringify({
+          anotherroute: "called",
+        }));
+        res.end();
+      },
+    },
+    athirdroute: {
+      handler: function(req, res, next, err, obj) {
+        res.writeHead(200, {'Content-Type': 'application/json' });
+        res.write(JSON.stringify({
+          athirdroute: "called",
+          object: obj
+        }));
+        res.end();
+      },
+      methods: ['get', 'post'],
+      detail: true,
+        }
   },
 });
 app.register({
