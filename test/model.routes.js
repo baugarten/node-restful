5115a469f5815d6d98884ade19bd0ed103e51c0b
Ben Augarten <b.augarten@gmail.com>
2013-01-28 16:15:39 -0800

    Refactored to make routing more consistent

--- test/model.routes.js
+++ test/model.routes.js
@@ -12,14 +12,14 @@ describe('Model', function() {
       movie.routes['get'].should.be.a('function');
       movie.routes['post'].should.be.a('function');
       movie.routes['put'].should.be.a('function');
-      movie.routes['schema'].should.be.a('function');
+      movie.routes['schema']['__all__'].should.be.a('function');
       movie.routes.should.not.have.property('delete');
 
       user.routes['get'].should.be.a('function');
       user.routes['post'].should.be.a('function');
       user.routes['put'].should.be.a('function');
       user.routes['delete'].should.be.a('function');
-      user.routes['schema'].should.be.a('function');
+      user.routes['schema']['__all__'].should.be.a('function');
     });
   });
 });
