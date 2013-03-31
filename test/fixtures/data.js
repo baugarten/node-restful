163bb5d04bb0eb68e998a88b0c813a070565efd0
Ben Augarten <b.augarten@gmail.com>
2013-03-30 20:28:57 -0700

    Added fixture data

---
+++ test/fixtures/data.js
@@ -0,0 +1,73 @@
+var ObjectId = require('mongodb').BSONNative.ObjectID;
+
+exports.users = [
+  {
+    _id: new ObjectId(),
+    username: "test",
+    pass_hash: 12374238719845134515,
+  },
+  {
+    _id: new ObjectId(),
+    username: "test2",
+    pass_hash: 1237987381263189273123,
+  }
+];
+
+exports.movies = [
+  {
+    _id: new ObjectId(),
+    title: "Title1",
+    year: 2012,
+    meta: {
+      productionco: "idk",
+      director: exports.users[1]._id,
+    },
+    creator: exports.users[0]._id,
+    secret: "A SECRET STRING",
+  },
+  {
+    _id: new ObjectId(),
+    title: "Title2",
+      year: 2011
+  },
+  {
+    _id: new ObjectId(),
+    title: "Title3",
+      year: 2013
+  },
+  {
+    _id: new ObjectId(),
+    title: "Title4",
+      year: 2013
+  },
+  {
+    _id: new ObjectId(),
+    title: "Title5",
+      year: 2013
+  },
+  {
+    _id: new ObjectId(),
+    title: "Title6",
+      year: 2013
+  },
+  {
+    _id: new ObjectId(),
+    title: "Title7",
+      year: 2013
+  },
+  {
+    _id: new ObjectId(),
+    title: "Title8",
+      year: 2013
+  },
+  {
+    _id: new ObjectId(),
+    title: "Title9",
+      year: 2013
+  },
+  {
+    _id: new ObjectId(),
+    title: "Title10",
+      year: 2013
+  }
+];
