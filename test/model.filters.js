aa9d8fbb8b157b1d0f753cc77d75c9703c7c4c22
Victor M <victor@canelatools.com>
2013-06-12 09:36:15 +0200

    added filter tests

--- test/model.filters.js
+++ test/model.filters.js
@@ -14,7 +14,7 @@ describe('Model', function() {
       user2;
   before(function(done) {
     config = require('./fixtures/config');
-    config.ready(function() { 
+    config.ready(function() {
       app = config.app;
       movies = config.movie;
       users = config.user;
@@ -53,6 +53,97 @@ describe('Model', function() {
               res.body[0].should.eql(res2.body[res2.body.length-1]);
               done();
             });
+        });
+    });
+    it('should select fields', function(done) {
+      request(app)
+        .get('/api/movies?select=year')
+        .end(function(err, res) {
+          res.body.forEach(function(movie) {
+            movie.should.not.have.property('title');
+            movie.should.have.property('year');
+          });
+          done();
+        });
+    });
+    it('should sort documents', function(done) {
+      request(app)
+        .get('/api/movies?sort=year')
+        .end(function(err, res) {
+          for(var i = 1; i < res.body.length; i++) {
+            (res.body[i].year >= res.body[i-1].year).should.be.true;
+          }
+          done();
+        });
+    });
+    it('should filter fields using equal', function(done) {
+      request(app)
+        .get('/api/movies?year=2011')
+        .end(function(err, res) {
+          res.body.forEach(function(movie) {
+            movie.year.should.equal(2011);
+          });
+          done();
+        });
+    });
+    it('should filter fields using gte', function(done) {
+      request(app)
+        .get('/api/movies?year__gte=2012')
+        .end(function(err, res) {
+          res.body.forEach(function(movie) {
+            movie.year.should.be.above(2011);
+          });
+          done();
+        });
+    });
+    it('should filter fields using gt', function(done) {
+      request(app)
+        .get('/api/movies?year__gt=2011')
+        .end(function(err, res) {
+          res.body.forEach(function(movie) {
+            movie.year.should.be.above(2011);
+          });
+          done();
+        });
+    });
+    it('should filter fields using lt', function(done) {
+      request(app)
+        .get('/api/movies?year__lt=2013')
+        .end(function(err, res) {
+          res.body.forEach(function(movie) {
+            movie.year.should.be.below(2013);
+          });
+          done();
+        });
+    });
+    it('should filter fields using lte', function(done) {
+      request(app)
+        .get('/api/movies?year__lte=2012')
+        .end(function(err, res) {
+          res.body.forEach(function(movie) {
+            movie.year.should.be.below(2013);
+          });
+          done();
+        });
+    });
+    it('should filter fields using ne', function(done) {
+      request(app)
+        .get('/api/movies?year__ne=2013')
+        .end(function(err, res) {
+          res.body.forEach(function(movie) {
+            movie.year.should.not.equal(2013);
+          });
+          done();
+        });
+    });
+    it('should filter fields using regex', function(done) {
+      request(app)
+        .get('/api/movies?title__regex=2')
+        .end(function(err, res) {
+          res.body.forEach(function(movie) {
+            movie.title.should.include('2');
+          });
+          done();
         });
     });
   });
