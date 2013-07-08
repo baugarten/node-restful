f22aec7db546a40bb1640df0aa284316436294c9
Ben Augarten <b.augarten@gmail.com>
2013-07-08 12:06:56 -0700

    Added test to demonstrate #33 works

--- test/model.chainable.js
+++ test/model.chainable.js
@@ -32,4 +32,15 @@ describe('Model', function() {
     posts.templateRoot.should.eql('/idk/where/this/goes');
     
   });
+  it('should be updateable', function() {
+    var Posts = restful.model('posts');
+    Posts.create({
+      title: "First post"
+    }, function(err, post) {
+      post.title.should.equal('First post');
+      Posts.update({_id: post._id}, { title: "Second post"}, function(err, post) {
+        post.title.should.equal('Second post');
+      });
+    })
+  });
 });
